cf1d203a93cb9467f38d1096e942953f56f978f8
hq5ricky <107004711+hq5ricky@users.noreply.github.com>
2022-06-07 20:07:36 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 let e = new PIXI.Application({ width: innerWidth, height: innerHeight });
 document.body.appendChild(e.view);
 
-let sprite = PIXI.Sprite.from('./sprites/brick0.png');
-e.stage.addChild(sprite);
+//let sprite = PIXI.Sprite.from('./sprites/brick0.png');
 
 const sprites = {};
 
 const loader = new PIXI.Loader();
+let a = new PIXI.Container;
 
 loader.add("./sprites/brick0.png").load(() => {
-  sprite.wall = new PIXI.TilingSprite(PIXI.Texture.from("brick0.png"));
+  sprite.wall = new PIXI.TilingSprite(PIXI.Texture.from("brick0.png")), this.wall.width = 100, this.wall.height = 100;
 });
